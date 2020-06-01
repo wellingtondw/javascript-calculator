61f7d325ec29a46fdec4fb688c6e900edd98445d
Wellington <wellingtonslpbi@hotmail.com>
2020-06-01 20:12:50 -0300

    render display, date and hour with js

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -3,6 +3,18 @@ class CalcController {
     constructor() {
         this._displayCalc = 0;
         this._currentDate;
+
+        this.initialize();
+    }
+
+    initialize() {
+        let displayCalcEl = document.querySelector('#display');
+        let dateCalcEl = document.querySelector('#date');
+        let timeCalcEl = document.querySelector('#hour');
+
+        displayCalcEl.innerHTML = '4567';
+        dateCalcEl.innerHTML = '01/06/2020';
+        timeCalcEl.innerHTML = '00:00';
     }
 
     get displayCalc() {
